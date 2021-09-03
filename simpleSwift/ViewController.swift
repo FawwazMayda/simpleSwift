1282a1f3a95fef1e1eb5714dee953acebaf19246
fawwazmayda <maydafawwaz@gmail.com>
2021-09-03 15:22:26 +0800

    Initial Commit

---
+++ simpleSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  simpleSwift
+//
+//  Created by Muhammad Fawwaz Mayda on 03/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
